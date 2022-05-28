bd996c3ae76c50b79e15e83b7d6a2509d04d9449
AzeemullahRg <61787698+AzeemullahRg@users.noreply.github.com>
2022-05-28 14:40:57 +0500

    initializing project

---
+++ HotelManagementSystem/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        public ProfileController()
+        {
+        }
+
+        [HttpGet]
+#pragma warning disable 1998
+        public async Task<string> Get()
+#pragma warning restore 1998
+        {
+            return "Test";
+        }
+    }
+}
